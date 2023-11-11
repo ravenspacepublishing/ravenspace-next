e6b6e34c44578a36149be8b2a227f1d3d8bd45e4
ravenspace <74627811+ravenspace@users.noreply.github.com>
2023-11-11 09:00:11 -0600

    Update _document.js

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -4,12 +4,13 @@ import config from "../../content/site.config.json";
 export default function Document() {
   return (
     <Html lang="en">
+      <html prefix="og: http://ogp.me/ns#">
       <Head>
+    
         {config.theme.themeColor && (
           <meta name="theme-color" content={config.theme.themeColor} />
         )}
         {config.noIndex && <meta name="robots" content="noindex, nofollow" />}
-        <html prefix="og: http://ogp.me/ns#">
         <meta property="og:type" content="website" />
         <meta property="og:site_name" content={config.title} />
         <meta name="twitter:card" content="summary_large_image" />
