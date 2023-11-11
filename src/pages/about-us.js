ae032b91027567cb779068fce19f1843a95fdc31
ravenspace <74627811+ravenspace@users.noreply.github.com>
2023-11-11 17:28:16 -0600

    Update about-us.js

--- src/pages/about-us.js
+++ src/pages/about-us.js
@@ -110,6 +110,7 @@ export default function AboutUs(props) {
           </div>
         </section>
         <section className="bg-gray-100 py-8 lg:py-14 px-4">
+         <div className="mt-6 mb-2 flex">      
             <Link
               className="w-full max-w-xs text-center text-white lg:text-lg mx-auto py-2 bg-rs-purple rounded-sm"
               href="/contact-us/"
