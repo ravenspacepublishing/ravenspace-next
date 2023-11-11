bf4714119d35b47080fe50065358072902edbb0e
ravenspace <74627811+ravenspace@users.noreply.github.com>
2023-11-11 11:25:07 -0600

    Update _document.js

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -4,13 +4,11 @@ import config from "../../content/site.config.json";
 export default function Document() {
   return (
     <Html lang="en">
-      <!-- <html prefix="og: http://ogp.me/ns#"> -->
       <Head>
         {config.theme.themeColor && (
           <meta name="theme-color" content={config.theme.themeColor} />
         )}
         {config.noIndex && <meta name="robots" content="noindex, nofollow" />}
-        <!-- <meta property="og:image" content="https://assets.tina.io/df87757b-c018-41f7-a4a6-643a17f6060e/images/favicon-192.png?fit=crop&max-w=400&max-h=400" /> -->
         <meta property="og:type" content="website" />
         <meta property="og:site_name" content={config.title} />
         <meta name="twitter:card" content="summary_large_image" />
