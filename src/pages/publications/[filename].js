df57c78427b15cac95b3967b31b789082ea927d8
ravenspace <74627811+ravenspace@users.noreply.github.com>
2025-02-13 13:35:18 -0600

    Update [filename].js

--- src/pages/publications/[filename].js
+++ src/pages/publications/[filename].js
@@ -150,7 +150,7 @@ export default function Page(props) {
                     <em>
                       For the best experience with this publication, use the
                       latest version of your web browser. If you encounter a
-                      problem, you can
+                      problem, you can 
                       <Link
                         className="underline"
                         style={{ color: "#5050bc" }}
